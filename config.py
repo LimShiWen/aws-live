0608074224907bdc95f3d957b015737c7393af29
LimShiWen <120999105+LimShiWen@users.noreply.github.com>
2022-12-19 11:45:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cfblozfr7bpz.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
